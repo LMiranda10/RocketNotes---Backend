d8c5d4545d201d625ed8643f19f28fe5808baae0
LMiranda <luan.miranda103@gmail.com>
2023-05-03 20:29:11 -0300

    SESSIONS E TOKEN

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,10 @@ const routes = Router();
 const usersRouter = require("./user.routes");
 const notesRouter = require("./notes.routes");
 const tagsRouter = require("./tags.routes");
+const sessionsRouter = require("./sessions.routes");
 
 routes.use("/users", usersRouter)
+routes.use("/sessions", sessionsRouter)
 routes.use("/notes", notesRouter)
 routes.use("/tags", tagsRouter)   
 
